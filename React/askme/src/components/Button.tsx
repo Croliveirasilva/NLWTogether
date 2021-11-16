ad7d6d1d76cff764b55ff4636a9018a0b3802260
Carlos Rafael <66805060+Croliveirasilva@users.noreply.github.com>
2021-11-16 09:28:28 -0300

    Inicio Aplicação
    Criação da Home, estilização , e componente de botão

---
+++ React/askme/src/components/Button.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { ButtonHTMLAttributes } from "react";
+import '../styles/button.scss';
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+
+export function Button(props: ButtonProps) {
+    return (
+        <button className="button" {...props}>
+
+        </button>
+    )
+}
