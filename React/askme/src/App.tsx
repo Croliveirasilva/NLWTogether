5bd9a11fbc2e3a8be2ff3706a95ed4ac87cbe076
Carlos Rafael <66805060+Croliveirasilva@users.noreply.github.com>
2021-11-23 08:37:31 -0300

    Atualização
    Atualização de dependências

--- React/askme/src/App.tsx
+++ React/askme/src/App.tsx
@@ -1,8 +1,16 @@
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
 
 function App() {
   return (
-    <Home />
+    <BrowserRouter>
+      <Route path="/" element={<Home />} />
+
+      <Route path="/room/new" element={<NewRoom />} />
+    </BrowserRouter>
+
   );
 }
 
